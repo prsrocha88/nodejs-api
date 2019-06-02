fe0f8351449ff4204bda6b24d46027a9b785b158
prsrocha88 <prsrocha88@gmail.com>
2019-06-02 17:02:39 +0100

    Use Post Model

--- controllers/feed.controller.js
+++ controllers/feed.controller.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator/check');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [{
@@ -25,17 +27,23 @@ exports.createPost = (req, res, next) => {
 
     const title = req.body.title;
     const content = req.body.content;
-
-    res.status(201).json({
-        message: 'Post created successfully',
-        post: {
-            _id: new Date().toISOString(),
-            title: title,
-            content: content,
-            creator: {
-                name: 'Paulo Rocha'
-            },
-            createdAt: new Date()
+    const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: 'images/duck.jpg',
+        creator: {
+            name: 'Paulo Rocha'
         }
     });
+
+    post.save().then((result) => {
+        res.status(201).json({
+            message: 'Post created successfully',
+            post: result
+        });
+    }).catch((err) => {
+        console.log(err);
+    });
+
+    
 };
