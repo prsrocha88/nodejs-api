a99115ab43feef7eddef6034459b584319056a0d
prsrocha88 <prsrocha88@gmail.com>
2019-06-08 19:12:30 +0100

    Update post

--- routes/api.js
+++ routes/api.js
@@ -22,4 +22,17 @@ router.post(
 
 router.get('/posts/:postId', feedController.getPost);
 
+router.put(
+    '/posts/:postId', 
+    [
+        body('title')
+            .trim()
+            .isLength({ min: 5 }),
+        body('content')
+            .trim()
+            .isLength({ min: 5 })
+    ], 
+    feedController.updatePost
+);
+
 module.exports = router;
