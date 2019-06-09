7d9fbf50e210fdb6405210f53b52e1b95f9300d4
prsrocha88 <prsrocha88@gmail.com>
2019-06-09 13:25:59 +0100

    Add pagination to GET /posts

--- controllers/feed.controller.js
+++ controllers/feed.controller.js
@@ -6,18 +6,29 @@ const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
+    const currentPage = req.query.page || 1;
+    const perPage = 2;
+    let totalItems;
     Post.find()
+        .countDocuments()
+        .then(count => {
+            totalItems = count;
+            return Post.find()
+                .skip((currentPage - 1) * perPage)
+                .limit(perPage);
+        })
         .then(result => {
             res.status(200).json({
                 message: 'Fetched posts successfully.',
-                posts: result
-            })
-            .catch((err) => {
-                if (!err.statusCode) {
-                    err.statusCode = 500;
-                }
-                next(err);
+                posts: result,
+                totalItems: totalItems
             });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
         });
 };
 
