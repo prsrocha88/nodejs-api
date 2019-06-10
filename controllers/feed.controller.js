60dac92604114a8a3c25fe6bbe98daf28f160d04
prsrocha88 <prsrocha88@gmail.com>
2019-06-10 18:39:24 +0100

    Remove posts from users collection

--- controllers/feed.controller.js
+++ controllers/feed.controller.js
@@ -169,9 +169,15 @@ exports.deletePost = (req, res, next) => {
             }
             clearImage(post.imageUrl);
             return Post.findByIdAndRemove(postId);
-        }).
-        then(result => {
-            console.log(result);
+        })
+        .then(result => {
+            return User.findById(req.userId);
+        })
+        .then(user => {
+            user.posts.pull(postId);
+            return user.save();
+        })
+        .then(result => {
             res.status(200).json({ message: 'Deleted post.' });
         })
         .catch(err => {
