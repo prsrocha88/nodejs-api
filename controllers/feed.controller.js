db8dd6e58555b7e035eba85bb898c14dec994076
prsrocha88 <prsrocha88@gmail.com>
2019-06-10 18:24:04 +0100

    Check user authorization

--- controllers/feed.controller.js
+++ controllers/feed.controller.js
@@ -132,6 +132,12 @@ exports.updatePost = (req, res, next) => {
                 error.statusCode = 404;
                 throw error;
             }
+            console.log(post.creator.toString(), req.userId);
+            if (post.creator.toString() !== req.userId) {
+                const error = new Error('Not authorized!');
+                error.statusCode = 404;
+                throw error;
+            }
             if (imageUrl !== post.imageUrl) {
                 clearImage(post.imageUrl);
             }
